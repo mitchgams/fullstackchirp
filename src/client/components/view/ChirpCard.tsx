8163fcfa2c0e06ad6ce17e68d046f9a689f19e2f
Mitchell Gams <mitchgams@gmail.com>
2020-12-16 11:42:59 -0600

    changed funct name in parseMentions.tsx to parse()

--- src/client/components/view/ChirpCard.tsx
+++ src/client/components/view/ChirpCard.tsx
@@ -22,7 +22,7 @@ const ChirpCard: React.FC<IAppProps> = ({chirps}) => {
             return (
                 <article key={chirpid} className="card m-2 p-0 shadow-sm">
                     <h5 className="card-title m-1"><a href={`/user/${username}`} style={{textDecoration: 'none'}}>{username}</a></h5>
-                    <p className="card-body m-0">{ReactHtmlParser (Mentions.mentions(content))}</p>
+                    <p className="card-body m-0">{ReactHtmlParser (Mentions.parse(content))}</p>
                     <div className="card-footer m-0 p-0 d-flex justify-content-end"><Link to={`/${chirpid}/admin`}>[Admin]</Link></div>
                 </article>
             );
