4995641c1f77ac99ff0ea882b18b66e1baf65880
Mitchell Gams <mitchgams@gmail.com>
2020-12-16 11:40:07 -0600

    final push

--- src/client/components/view/Chirps/index.tsx
+++ src/client/components/view/Chirps/index.tsx
@@ -1,8 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import ReactHtmlParser from 'react-html-parser'; // React was showing raw html, found this solution
 import { useState, useEffect } from 'react';
-import * as Mentions from '../../parseMentions';
 import ChirpCard from '../ChirpCard';
 
 export interface IChirps {
@@ -27,21 +24,8 @@ const Chirps: React.FC = () => {
         })();
      }, []);
 
-    /*return (
-        <>
-            {chirps?.map(chirp => {
-                const { chirpid, username, content } = chirp;
-            return (
-                <article key={chirpid} className="card m-2 p-0 shadow-sm">
-                    <h5 className="card-title m-1"><a href={`/user/${username}`} style={{textDecoration: 'none'}}>{username}</a></h5>
-                    <p className="card-body m-0">{ReactHtmlParser (Mentions.mentions(content))}</p>
-                    <div className="card-footer m-0 p-0 d-flex justify-content-end"><Link to={`/${chirpid}/admin`}>[Admin]</Link></div>
-                </article>
-            );
-        })}
-        </>*/
-        return <ChirpCard chirps={chirps} />; 
-    //);
+    return <ChirpCard chirps={chirps} />; 
+
 }
 
 export default Chirps;
